99e527795925e3c2635dc10c03fb3badd6f864a4
Teemu Risikko <teemu.risikko@gmail.com>
2020-05-10 12:03:44 +0300

    Add travel

--- app/TravelAgency.cxx
+++ app/TravelAgency.cxx
@@ -7,11 +7,13 @@
 #include "agencybuilder.hpp"
 #include "customerbuilder.hpp"
 #include "driverbuilder.hpp"
-#include "trip.hpp"
+#include "travelbuilder.hpp"
 
 auto main() -> int
 {
     try {
+        std::cout << "Loading database...";
+
         tsv::Table<Agency> agencies("agencies");
         agencies.populate("agencies.txt", AgencyBuilder::build);
 
@@ -21,25 +23,52 @@ auto main() -> int
         tsv::Table<Driver> drivers("drivers");
         drivers.populate("drivers.txt", DriverBuilder::build);
 
-        tsv::Query<Agency, std::string> querya("Agency#7", &Agency::getName);
+        tsv::Table<Travel> travels("travels");
+        travels.populate("travels.txt", TravelBuilder::build);
 
+        std::cout << " done." << std::endl;
+
+        while(true) {
+            std::cout << "Enter agency name: ";
+
+            std::string agencyName;
+            std::cin >> agencyName;
+
+            tsv::Query<Agency, std::string> query(agencyName, &Agency::getName);
+            for(auto& agency : agencies.select(query)) {
+                const std::tm& time = std::tm(agency.getRegisteredDate());
+
+                std::cout << "ID:\t\t" << agency.getID() << std::endl
+                    << "Name:\t\t" << agency.getName() << std::endl
+                    << "Registered:\t" << std::put_time(&time, "%Y.%m.%d")  << std::endl
+                    << "Staff count:\t" << agency.getStaffCount() << std::endl
+                    << "Chief:\t\t" << agency.getChiefName() << std::endl
+                    << std::endl;
+            } 
+        }
+
+        /*
+        tsv::Query<Agency, std::string> querya("Agency#7", &Agency::getName);
         for(auto& agency : agencies.select(querya)) {
             std::cout << agency.getName() << " " << agency.getStaffCount() << std::endl;
         }
 
         tsv::Query<Driver, std::string> queryd("Grit Dixus", &Driver::getCarModel);
-
         for(auto& driver : drivers.select(queryd)) {
             std::cout << driver.getName() << " " << driver.getCarModel() << std::endl;
         }
 
         const int TEST_CUSTOMER_ID = 9031;
         tsv::Query<Customer, int> queryc(TEST_CUSTOMER_ID, &Customer::getID);
-
         for(auto& customer : customers.select(queryc)) {
             std::cout << customer.getName() << " " << customer.getPhone() << std::endl;
         }
 
+        const int TEST_TRAVEL_ID = 8274;
+        tsv::Query<Travel, int> queryt(TEST_TRAVEL_ID, &Travel::getID);
+        for(auto& travel : travels.select(queryt)) {
+            std::cout << travel.getOrigin() << " -> " << travel.getDestination() << std::endl;
+        }*/
 
     } catch (...) {
         return 0;
